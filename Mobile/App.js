114128096cde755c4127674deafe2c97a2513635
AlessandroDias1979 <alessandroddigital@gmail.com>
2026-03-08 09:04:15 -0300

    Parametros para inicialização
    Troca de parâmetros para inicialição para criação de brunch

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "./Home";
+import CadastroAluno from "./CadastroAluno";
+import CadastroTurma from "./CadastroTurma";
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name='Home' component={Home} />
+            <Stack.Screen name='CadastroAluno' component={CadastroAluno} />
+            <Stack.Screen name='CadastroTurma' component={CadastroTurma} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+/*Criar um novo APP que tenha um menu com uma tela de cadastro de aluno e outra tela de cadastro de turma.
+
+A tela de cadastro de Aluno deve poder cadastrar os  seguintes dados:
+
+Nome, Data de Nascimento , Sexo, Endereço
+
+A tela de Cadastro de Turma deve poder cadastrar os seguintes dados:
+
+Nome da Turma,  Ano,  Curso, Turno*/
