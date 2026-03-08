09181dfd43ba7956e1df7397765040e80ac7c10b
AlessandroDias1979 <alessandroddigital@gmail.com>
2026-03-08 20:37:13 -0300

    Ultima parte
    Começando o cadastro aluno

---
+++ Mobile/CadastroAluno/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import {View, Alert, Text, TextInput, Button,Platform, Switch,
+} from "react-native";
+import Estilo from "../Estilo";
+import { MaskedTextInput } from "react-native-mask-text";
+import DateTimePicker from "@react-native-community/datetimepicker"; // default import correto
+import { MultipleSelectList, SelectList } from "react-native-dropdown-select-list";
+
+export default function CadastroAluno(props) {
+
+    const [nome, setNome] = useState("");
+ 
+  }
+
+  return (
+    <View style={Estilo?.container}>
+    
+      <TextInput
+        label="Nome do Aluno"
+        Estilo={Estilo.input}
+        setcalor = {setNome}
+        textoaoiniciarl = "Digite o nome do aluno"
+        value={nome}
+        maxLength={100}
+      />
+
+      <Text></Text>
+
+      
+    </View>
+  );
+
+``
---
+++ Mobile/Home/index.js
@@ -0,0 +1,31 @@
+
+import React from "react";
+import { Text, View, TouchableOpacity, Image } from "react-native";
+import Estilo from "../Estilo"; // assumindo que Estilo é um StyleSheet export default
+
+export default function Home({ navigation }) {
+  const AbrirTelaCadastroAluno = () => {
+    navigation.navigate("CadastroAluno");
+  };
+
+  const AbrirTelaCadastroTurma = () => {
+    navigation.navigate("CadastroTurma");
+  };
+
+  return (
+    <View style={Estilo.container}>
+
+      <Image source={require("../assets/logo.png")} style={Estilo.Image} />
+      
+      <Text style={Estilo.titulo}>Home</Text>
+
+      <TouchableOpacity style={Estilo.Button} onPress={AbrirTelaCadastroAluno}>
+        <Text style={Estilo.ButtonTexto}>Cadastro de Aluno</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={Estilo.Button} onPress={AbrirTelaCadastroTurma}>
+        <Text style={Estilo.ButtonTexto}>Cadastro de Turma</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
